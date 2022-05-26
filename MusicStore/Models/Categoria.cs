cdef54a3766f444bbaabb5e45654e586421d9353
davidepetrillo <davide96petrillo@gmail.com>
2022-05-26 17:17:57 +0200

    Modifica modello categoria

--- MusicStore/Models/Categoria.cs
+++ MusicStore/Models/Categoria.cs
@@ -3,17 +3,17 @@
 namespace MusicStore.Models
 {
     public class Categoria
-{
+    {
         [Key]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Il campo è obbligatorio")]
         [StringLength(50, ErrorMessage = "Il nome della categoria non può superare i 50 caratteri")]
         public string nomeCategoria { get; set; }
-        public List<StrumentiMusicali> { get; set; }
 
-    public Categoria()
-            {
+        public Categoria()
+        {
 
-            }
+        }
     }
+}
