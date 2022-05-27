376a74bbf7cb4e9b4a547aa64f26a3f1e9d4c19f
P.angelino <p.angelinodaniele@gmail.com>
2022-05-27 15:28:17 +0200

    Migrations ChiaveEsterna di utenteId

--- MusicStore/Models/Utente.cs
+++ MusicStore/Models/Utente.cs
@@ -14,11 +14,12 @@ namespace MusicStore.Models
 
         [Required(ErrorMessage = "Il campo 'Quantità' è obbligatorio")]
         [Range(1,10,ErrorMessage ="Puoi acquistare un minimo di 1 prodotto/massimo di 10 prodotti")]
-        public int quantità { get; set; }
+        public int Quantità { get; set; }
 
         public DateTime Data { get; set; }
 
-        public List<StrumentoMusicale> StrumentiMusicaliUtente { get; set; }
+        public StrumentoMusicale StrumentoMusicale { get; set; }
+        public int StrumentoMusicaleId { get; set; }
 
         public Utente()
         {
