be54d5e96bd5f78db1434348050e8fb5df1ff7ea
Gennaro <gennarinodi02@gmail.com>
2022-05-27 10:28:16 +0200

    add StrumentoMusicale Controller+bozza view crea

---
+++ MusicStore/Controllers/StrumentoMusicaleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StrumentoMusicaleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
