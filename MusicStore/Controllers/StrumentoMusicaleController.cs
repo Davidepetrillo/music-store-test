3858714181ed5debe7467e92249890df18789b9d
Gennaro <gennarinodi02@gmail.com>
2022-05-27 11:02:23 +0200

    add view+controller Crea ed Index (ADMIN)

--- MusicStore/Controllers/StrumentoMusicaleController.cs
+++ MusicStore/Controllers/StrumentoMusicaleController.cs
@@ -6,6 +6,7 @@ namespace MusicStore.Controllers
 {
     public class StrumentoMusicaleController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
 
@@ -15,7 +16,41 @@ namespace MusicStore.Controllers
                 StrumentiMusicali = db.StrumentoMusicale.ToList<StrumentoMusicale>();
                 return View("HomeADMIN", StrumentiMusicali);
             }
-            
+
         }
+
+        //-----------------------------CREA------------------------------------------
+
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("CreaADMIN");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(StrumentoMusicale nuovoStrumentoMusicale)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CreaADMIN", nuovoStrumentoMusicale);
+            }
+
+            using (MusicContext db = new MusicContext())
+            {
+
+
+               StrumentoMusicale StrumentoDaCreare = new StrumentoMusicale(nuovoStrumentoMusicale.Nome, nuovoStrumentoMusicale.Descrizione, nuovoStrumentoMusicale.Foto, nuovoStrumentoMusicale.Prezzo, nuovoStrumentoMusicale.QuantitaStrumento);
+
+                db.StrumentoMusicale.Add(StrumentoDaCreare);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
