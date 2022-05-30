edacf2995b63c1583cbf259d3081285404e1dc6e
Gennaro <gennarinodi02@gmail.com>
2022-05-30 11:23:27 +0200

    add  classe rifornisci (no FK)

---
+++ MusicStore/Models/Rifornisci.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore.Models
+{
+    public class Rifornisci
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        DateTime data;
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(50, ErrorMessage = "Il nome della categoria non può superare i 50 caratteri")]
+        
+        public int quantita { get; set; }
+
+        public Rifornisci()
+        {
+
+        }
+        public Rifornisci(int quantità,DateTime data)
+        {
+            this.data = data;
+            data=DateTime.Now;
+        }
+    }
+}
